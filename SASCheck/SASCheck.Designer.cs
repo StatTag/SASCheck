438aef65500bddef3e6a9292639614df92e4cc56
Luke Rasmussen <luke.rasmussen@gmail.com>
2022-06-02 09:01:31 -0500

    Initial add

---
+++ SASCheck/SASCheck.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace SASCheck
+{
+    partial class SASCheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SASCheck));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdRun = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.cmdCopy = new System.Windows.Forms.Button();
+            this.lblCopied = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(628, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // cmdRun
+            // 
+            this.cmdRun.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cmdRun.Location = new System.Drawing.Point(248, 90);
+            this.cmdRun.Name = "cmdRun";
+            this.cmdRun.Size = new System.Drawing.Size(156, 23);
+            this.cmdRun.TabIndex = 1;
+            this.cmdRun.Text = "Run Diagnostic Check";
+            this.cmdRun.UseVisualStyleBackColor = true;
+            this.cmdRun.Click += new System.EventHandler(this.cmdRun_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResults.Location = new System.Drawing.Point(15, 129);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(625, 371);
+            this.txtResults.TabIndex = 2;
+            // 
+            // cmdCopy
+            // 
+            this.cmdCopy.Location = new System.Drawing.Point(590, 100);
+            this.cmdCopy.Name = "cmdCopy";
+            this.cmdCopy.Size = new System.Drawing.Size(50, 23);
+            this.cmdCopy.TabIndex = 3;
+            this.cmdCopy.Text = "Copy";
+            this.cmdCopy.UseVisualStyleBackColor = true;
+            this.cmdCopy.Click += new System.EventHandler(this.cmdCopy_Click);
+            // 
+            // lblCopied
+            // 
+            this.lblCopied.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblCopied.AutoSize = true;
+            this.lblCopied.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblCopied.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopied.Location = new System.Drawing.Point(589, 82);
+            this.lblCopied.Name = "lblCopied";
+            this.lblCopied.Size = new System.Drawing.Size(52, 16);
+            this.lblCopied.TabIndex = 4;
+            this.lblCopied.Text = "Copied";
+            this.lblCopied.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCopied.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 512);
+            this.Controls.Add(this.lblCopied);
+            this.Controls.Add(this.cmdCopy);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.cmdRun);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SAS Check";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdRun;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button cmdCopy;
+        private System.Windows.Forms.Label lblCopied;
+    }
+}
+
---
+++ SASCheck/SASCheck.cs
@@ -0,0 +1,344 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SASCheck
+{
+    public partial class SASCheck : Form
+    {
+        public SASCheck()
+        {
+            InitializeComponent();
+            cmdCopy.Enabled = false;
+            lblCopied.Visible = false;
+        }
+
+        private void cmdRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                cmdRun.Enabled = false;
+                cmdCopy.Enabled = false;
+
+                txtResults.Text = "";
+
+                CheckSASInstallation();
+                CheckTypesByID();
+                CheckTypesInRegistry();
+                CheckConnection();
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+                cmdRun.Enabled = true;
+                cmdCopy.Enabled = true;
+            }
+
+        }
+
+        private void AppendResults(string text)
+        {
+            if (text == null)
+            {
+                return;
+            }
+            txtResults.AppendText(text + "\r\n");
+        }
+
+        private void AppendException(Exception exc)
+        {
+            AppendResults("Exception caught:");
+            AppendResults(exc.Message);
+            AppendResults(exc.StackTrace);
+            if (exc.InnerException != null)
+            {
+                AppendResults("Inner exception:");
+                AppendResults(exc.InnerException.Message);
+                AppendResults(exc.InnerException.StackTrace);
+            }
+        }
+
+        private void CheckTypesByID()
+        {
+            try
+            {
+                AppendResults("Checking types");
+                CheckTypeByProgID("SAS.Application", "SAS Application");
+                CheckTypeByCLSID("89FA3E2A-43F9-43e4-B1A2-DAC2CC90B89C", "SAS Application");
+                CheckTypeByProgID("SASObjectManager.ObjectFactoryMulti2", "SAS Object Manager");
+                CheckOManObjectFactory();
+                CheckOManServerDef();
+                CheckTypeByProgID("SAS.Workspace", "SAS Workspace");
+                CheckTypeByCLSID("20001C16-05D1-4706-9BD2-1782B8575063", "SAS Workspace");
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckTypesInRegistry()
+        {
+            try
+            {
+                AppendResults("Checking type registry keys");
+                CheckSubKey(Registry.ClassesRoot, "SAS.Application", true, true);
+                CheckSubKey(Registry.ClassesRoot, "SAS.Application.940", true, true);
+                CheckSubKey(Registry.ClassesRoot, "SAS.Workspace", true, true);
+
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckSubKey(RegistryKey rootKey, string key, bool checkCLSID = false, bool printDefaultValue = false)
+        {
+            try
+            {
+                AppendResults(string.Format("Attempting to open {0} {1}", rootKey.Name, key));
+                var subKey = rootKey.OpenSubKey(key);
+                if (subKey == null)
+                {
+                    AppendResults("...Not found");
+                }
+                else
+                {
+                    AppendResults(string.Format("...Found"));
+                    if (checkCLSID)
+                    {
+                        CheckSubKey(subKey, "CLSID", false, true);
+                    }
+
+                    if (printDefaultValue)
+                    {
+                        AppendResults(string.Format("... With default value {0}", subKey.GetValue("")));
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckOManObjectFactory()
+        {
+            try
+            {
+                AppendResults("Attempting to create SAS Object Manager ObjectFactoryMulti2");
+                SASObjectManager.IObjectFactory2 obObjectFactory = new SASObjectManager.ObjectFactoryMulti2();
+                if (obObjectFactory == null)
+                {
+                    AppendResults("... NULL object");
+                }
+                else
+                {
+                    AppendResults("... created");
+                }
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckOManServerDef()
+        {
+            try
+            {
+                AppendResults("Attempting to create SAS Object Manager ServerDef");
+                SASObjectManager.ServerDef obServer = new SASObjectManager.ServerDef();
+                if (obServer == null)
+                {
+                    AppendResults("... NULL object");
+                }
+                else
+                {
+                    AppendResults("... created");
+                }
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckTypeByProgID(string progId, string description)
+        {
+            try
+            {
+                AppendResults(string.Format("Checking ProgID {0} ({1})", description, progId));
+                var type = Type.GetTypeFromProgID(progId, true);
+                if (type == null)
+                {
+                    AppendResults("...Type is NULL");
+                }
+                else
+                {
+                    AppendResults("... Found");
+                }
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckTypeByCLSID(string clsid, string description)
+        {
+            try
+            {
+                AppendResults(string.Format("Checking CLSID {0} ({1})", description, clsid));
+                var type = Type.GetTypeFromCLSID(new Guid(clsid));
+                if (type == null)
+                {
+                    AppendResults("...Type is NULL");
+                }
+                else
+                {
+                    AppendResults("... Found");
+                }
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckSASInstallation()
+        {
+            try
+            {
+                AppendResults("Checking SAS Installation");
+                var sasHome = ConfigurationManager.AppSettings["SASLocation"];
+                AppendResults(string.Format("Checking SAS Home: {0}", sasHome));
+                if (Directory.Exists(sasHome))
+                {
+                    AppendResults("... Found");
+                }
+                else
+                {
+                    AppendResults(string.Format("... SAS Home not found"));
+                }
+
+                var sasIT = Path.Combine(sasHome, "Integration Technologies");
+                AppendResults(string.Format("Checking: {0}", sasIT));
+                if (Directory.Exists(sasIT))
+                {
+                    AppendResults("... Found");
+                }
+                else
+                {
+                    AppendResults(string.Format("... SAS Integration Technologies not found"));
+                }
+
+                var sasObjMan = Path.Combine(sasIT, "SASOMan.dll");
+                AppendResults(string.Format("Checking: {0}", sasObjMan));
+                if (File.Exists(sasObjMan))
+                {
+                    AppendResults("... Found");
+                }
+                else
+                {
+                    AppendResults(string.Format("... SAS Object Manager DLL not found"));
+                }
+
+                var sasITx86 = Path.Combine(sasHome, "x86\\Integration Technologies");
+                AppendResults(string.Format("Checking: {0}", sasITx86));
+                if (Directory.Exists(sasITx86))
+                {
+                    AppendResults("... Found");
+                }
+                else
+                {
+                    AppendResults(string.Format("... SAS Integration Technologies (x86) DLL not found"));
+                }
+
+                var sasTlb = Path.Combine(sasITx86, "sas.tlb");
+                AppendResults(string.Format("Checking: {0}", sasTlb));
+                if (File.Exists(sasTlb))
+                {
+                    AppendResults("... Found");
+                }
+                else
+                {
+                    AppendResults(string.Format("... SAS Type Library not found"));
+                }
+
+                var sasIOMCommon = Path.Combine(sasITx86, "SASIOMCommon.tlb");
+                AppendResults(string.Format("Checking: {0}", sasIOMCommon));
+                if (File.Exists(sasIOMCommon))
+                {
+                    AppendResults("... Found");
+                }
+                else
+                {
+                    AppendResults(string.Format("... SAS IOM Common Type Library not found"));
+                }
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void CheckConnection()
+        {
+            try
+            {
+                AppendResults("Attemping full SAS connection");
+                AppendResults("Creating object keeper");
+                SASObjectManager.ObjectKeeper objectKeeper = new SASObjectManager.ObjectKeeper();
+                // Connect using COM protocol, locally installed SAS only
+                AppendResults("Creating object factory");
+                SASObjectManager.IObjectFactory2 obObjectFactory = new SASObjectManager.ObjectFactoryMulti2();
+                AppendResults("Creating server definition");
+                SASObjectManager.ServerDef obServer = new SASObjectManager.ServerDef();
+                obServer.MachineDNSName = "localhost";
+                obServer.Protocol = SASObjectManager.Protocols.ProtocolCom;
+                obServer.Port = 0;
+                AppendResults("Creating workspace");
+                var workspace = (SAS.Workspace)obObjectFactory.CreateObjectByServer(Name, true, obServer, null, null);
+
+                AppendResults("Adding workspace to object keeper");
+                objectKeeper.AddObject(1, "Test", workspace);
+
+                AppendResults("Closing workspace");
+                workspace.Close();
+
+                AppendResults("Clearing the object keeper");
+                objectKeeper.RemoveAllObjects();
+
+                workspace = null;
+                AppendResults("...Success");
+            }
+            catch (Exception exc)
+            {
+                AppendException(exc);
+            }
+        }
+
+        private void cmdCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtResults.Text);
+            lblCopied.Visible = true;
+
+            var timer = new Timer();
+            timer.Interval = 3000;
+            timer.Tick += (source, te) => { lblCopied.Visible = false; timer.Stop(); };
+            timer.Start();
+        }
+    }
+}
